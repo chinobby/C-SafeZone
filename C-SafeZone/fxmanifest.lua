6213e0dab3e048bbb2de9783c90680fef8736235
chinooo <danielitozmora@gmail.com>
2025-03-22 17:53:19 +0000

    Add files via upload

---
+++ C-SafeZone/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'C-SafeZone'
+author 'Chinooo'
+version '1.0.0'
+
+shared_script{
+    'Shared/config.lua',
+    '@es_extended/imports.lua'
+}
+
+client_scripts {
+    'Client/client.lua'
+}
